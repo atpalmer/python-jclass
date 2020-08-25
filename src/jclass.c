7012fe5e68b7646472927e7760d5d2fe52914c5b
Andrew Palmer <atp@sdf.org>
2020-08-25 00:23:44 -0500

    Add version fields

--- src/jclass.c
+++ src/jclass.c
@@ -2,10 +2,30 @@
 
 /* TODO: make PyObject */
 typedef struct {
-    uint8_t *magic_number;  /* ptr to .data */
+    /* ptrs to .data[] (big-endian order!) */
+    uint8_t *magic_number;
+    uint8_t *minor_version;
+    uint8_t *major_version;
+
     Py_ssize_t size;
     uint8_t data[];
 } JavaClass;
+
+static uint16_t deref16(uint8_t *val) {
+    uint8_t result[2];
+    result[0] = val[1];
+    result[1] = val[0];
+    return *(uint16_t *)&result;
+}
+
+static uint32_t deref32(uint8_t *val) {
+    uint8_t result[4];
+    result[0] = val[3];
+    result[1] = val[2];
+    result[2] = val[1];
+    result[3] = val[0];
+    return *(uint32_t *)&result;
+}
 
 static PyObject *jclass_load(PyObject *self, PyObject *args) {
     char *fname;
@@ -19,12 +39,11 @@ static PyObject *jclass_load(PyObject *self, PyObject *args) {
     fclose(fp);
 
     class->magic_number = &class->data[0];
+    class->minor_version = &class->data[4];
+    class->major_version = &class->data[6];
 
-    printf("Magic Number: %X%X%X%X\n",
-        class->magic_number[0],
-        class->magic_number[1],
-        class->magic_number[2],
-        class->magic_number[3]);
+    printf("Magic Number: %X\n", deref32(class->magic_number));
+    printf("Version: %u.%u\n", deref16(class->major_version), deref16(class->minor_version));
 
     PyObject *result = PyBytes_FromStringAndSize(class->data, class->size);
     PyMem_Free(class);

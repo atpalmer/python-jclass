3f6e19e81c80262ca3f50a896cc6a93ec7246fbb
Andrew Palmer <atp@sdf.org>
2020-09-11 17:33:14 -0500

    Factor out constant_pool.h

---
+++ src/constant_pool.h
@@ -0,0 +1,104 @@
+#ifndef CONSTANT_POOL_H
+#define CONSTANT_POOL_H
+
+#import "common.h"
+
+enum constant_type {
+    CONSTANT_TYPE_Utf8= 1,
+    CONSTANT_TYPE_Class = 7,
+    CONSTANT_TYPE_String = 8,
+    CONSTANT_TYPE_Fieldref = 9,
+    CONSTANT_TYPE_Methodref = 10,
+    CONSTANT_TYPE_InterfaceMethodref = 11,
+    CONSTANT_TYPE_NameAndType = 12,
+};
+
+static const char *Constant_description[] = {
+    [0] = "",
+    [1] = "Utf8",
+    [2] = NULL,
+    [3] = NULL,
+    [4] = NULL,
+    [5] = NULL,
+    [6] = NULL,
+    [7] = "Class",
+    [8] = "String",
+    [9] = "Fieldref",
+    [10] = "Methodref",
+    [11] = "InterfaceMethodref",
+    [12] = "NameAndType",
+    [13] = NULL,
+    [14] = NULL,
+    [15] = NULL,
+    [16] = NULL,
+};
+
+
+/* common header byte */
+
+static inline uint8_t Constant_tag(void *head) {
+    return UINT8_AT(head, 0);
+}
+
+
+/* 1 = Utf8 */
+
+static inline uint16_t Utf8_length(void *head) {
+    return UINT16_AT(head, 1);
+}
+
+static inline char *Utf8_bytes(void *head) {
+    return POINTER_AT(head, 3);
+}
+
+
+/* 7 = Class */
+
+static inline uint16_t Class_name_index(void *head) {
+    return UINT16_AT(head, 1);
+}
+
+
+/* 8 = String */
+
+static inline uint16_t String_string_index(void *head) {
+    return UINT16_AT(head, 1);
+}
+
+
+/* 9 = Fieldref */
+
+static inline uint16_t Fieldref_class_index(void *head) {
+    return UINT16_AT(head, 1);
+}
+
+static inline uint16_t Fieldref_name_and_type_index(void *head) {
+    return UINT16_AT(head, 3);
+}
+
+
+/* 10 = Methodref */
+
+static inline uint16_t Methodref_class_index(void *head) {
+    return UINT16_AT(head, 1);
+}
+
+static inline uint16_t Methodref_name_and_type_index(void *head) {
+    return UINT16_AT(head, 3);
+}
+
+
+/* 12 = NameAndType */
+
+static inline uint16_t NameAndType_name_index(void *head) {
+    return UINT16_AT(head, 1);
+}
+
+static inline uint16_t NameAndType_descriptor_index(void *head) {
+    return UINT16_AT(head, 3);
+}
+
+
+#define CONSTANT_DESC(c)    (Constant_description[Constant_tag(c)])
+
+#endif

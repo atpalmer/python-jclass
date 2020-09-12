9fdab914a42fdf758b4cce6cd935d049bda4336f
Andrew Palmer <atp@sdf.org>
2020-09-12 00:08:26 -0500

    Add UINT32 macros

--- src/common.h
+++ src/common.h
@@ -3,9 +3,11 @@
 
 #define UINT8(p)    (*(p))
 #define UINT16(p)   (ntohs(*(uint16_t *)p))
+#define UINT32(p)   (ntohl(*(uint32_t *)p))
 
 #define UINT8_AT(p, i)      UINT8(&(((uint8_t *)(p))[(i)]))
 #define UINT16_AT(p, i)     UINT16(&(((uint8_t *)(p))[(i)]))
+#define UINT32_AT(p, i)     UINT32(&(((uint8_t *)(p))[(i)]))
 #define POINTER_AT(p, i)    ((void *)&(((uint8_t *)(p))[(i)]))
 
 #endif

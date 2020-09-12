c1bce6d273c14de94731edbbef73e96cbcceea3e
Andrew Palmer <atp@sdf.org>
2020-09-11 19:48:19 -0500

    Set fields pointers

--- src/jclass.c
+++ src/jclass.c
@@ -27,7 +27,7 @@ typedef struct {
     uint16_t *this_class;
     uint16_t *super_class;
     uint16_t *interfaces_count;
-    uint8_t *interfaces;
+    uint16_t *interfaces;  /* interface_indexes */
 
     uint16_t *fields_count;
     uint8_t *fields;
@@ -135,7 +135,11 @@ static PyObject *jclass_load(PyObject *self, PyObject *args) {
     class->interfaces_count = NEXT_PTR(class->super_class);
     printf("Interfaces count: %u\n", ntohs(*class->interfaces_count));
 
-    class->interfaces = NEXT_PTR(*class->interfaces_count);
+    class->interfaces = NEXT_PTR(class->interfaces_count);
+    /* interfaces items are fixed-width; skip parsing for now */
+
+    class->fields_count = class->interfaces + *class->interfaces_count;
+    class->fields = NEXT_PTR(class->fields_count);
 
     PyObject *result = PyBytes_FromStringAndSize(class->data, class->size);
     PyMem_Free(class);

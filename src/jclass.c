2ba8ae64933f1d12cb840b0a790e7528e46a18e0
Andrew Palmer <atp@sdf.org>
2020-09-15 15:08:58 -0500

    Break out JavaClass constructor

--- src/jclass.c
+++ src/jclass.c
@@ -32,6 +32,27 @@ static void _JavaClass_free(JavaClass *this) {
     PyMem_Free(this);
 }
 
+static JavaClass *JavaClass_from_MemReader(MemReader *r) {
+    JavaClass *class = PyMem_Malloc(sizeof(*class));
+
+    class->magic_number = MemReader_next_uint32(r);
+    class->minor_version = MemReader_next_uint16(r);
+    class->major_version = MemReader_next_uint16(r);
+
+    constant_pool_parse(r, &class->constant_pool);
+
+    class->access_flags = MemReader_next_uint16(r);
+    class->this_class = MemReader_next_uint16(r);
+    class->super_class = MemReader_next_uint16(r);
+
+    interfaces_parse(r, &class->interfaces);
+    fields_parse(r, &class->fields);
+    parse_methods(r, &class->methods);
+    attributes_parse(r, &class->attributes);
+
+    return class;
+}
+
 static void JavaClass_print(JavaClass *class) {
     printf("Magic Number: %X\n", class->magic_number);
     printf("Version: %u.%u\n", class->major_version, class->minor_version);
@@ -52,23 +73,7 @@ static PyObject *jclass_load(PyObject *self, PyObject *args) {
         return NULL;
 
     MemReader *r = MemReader_from_filename(fname);
-    JavaClass *class = PyMem_Malloc(sizeof(*class));
-
-    class->magic_number = MemReader_next_uint32(r);
-    class->minor_version = MemReader_next_uint16(r);
-    class->major_version = MemReader_next_uint16(r);
-
-    constant_pool_parse(r, &class->constant_pool);
-
-    class->access_flags = MemReader_next_uint16(r);
-    class->this_class = MemReader_next_uint16(r);
-    class->super_class = MemReader_next_uint16(r);
-
-    interfaces_parse(r, &class->interfaces);
-    fields_parse(r, &class->fields);
-    parse_methods(r, &class->methods);
-    attributes_parse(r, &class->attributes);
-
+    JavaClass *class = JavaClass_from_MemReader(r);
     JavaClass_print(class);
     _JavaClass_free(class);
 

0cbe6c9602a0bbd2da24d3ad06b66f5297a151b7
Andrew Palmer <atp@sdf.org>
2020-09-14 22:37:03 -0500

    Use MemReader API in fields parser

--- src/fields.c
+++ src/fields.c
@@ -11,31 +11,30 @@
 
 
 void fields_parse(MemReader *reader, JavaClassFields **obj) {
-    uint8_t *data = MEMREADER_CURR(reader);
     size_t curr_bytes = 0;
 
-    uint16_t count;
-    curr_bytes += parse16(&data[curr_bytes], &count);
+    uint16_t count = MemReader_next_uint16(reader);
 
     *obj = PyMem_Malloc(sizeof(JavaClassFields) + (sizeof(JavaClassField *) * count));
     (*obj)->fields_count = count;
 
     for(uint16_t i = 0; i < count; ++i) {
-        uint8_t *p = &data[curr_bytes];
+        uint8_t *p = MEMREADER_CURR(reader);
 
         JavaClassField **field = &(*obj)->fields[i];
         JavaClassAttributes *attributes;
+
         size_t attr_size = attributes_parse(FIELD_ATTR_OBJ_PTR(p), &attributes);
+
         *field = PyMem_Malloc(sizeof(JavaClassField) + (sizeof(JavaClassAttribute *) * attributes->attributes_count));
         (*field)->access_flags = Field_access_flags(p);
         (*field)->name_index = Field_name_index(p);
         (*field)->descriptor_index = Field_descriptor_index(p);
         (*field)->attributes = attributes;
-        curr_bytes += attr_size;
-        curr_bytes += 6;  /* exclude sizeof(attributes_count) */
-    }
 
-    reader->pos += curr_bytes;
+        reader->pos += attr_size;
+        reader->pos += 6;  /* exclude sizeof(attributes_count) */
+    }
 }
 
 void fields_print(JavaClassFields *this) {

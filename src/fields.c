3159462543c7d463d2f520bf10728f667451233b
Andrew Palmer <atp@sdf.org>
2020-09-17 22:41:59 -0500

    Error check memory allocation

--- src/fields.c
+++ src/fields.c
@@ -8,12 +8,16 @@
 struct field_items *fields_parse(MemReader *reader) {
     uint16_t count = MemReader_next_uint16(reader);
     struct field_items *obj = calloc(1, sizeof(struct field_items) + (sizeof(struct field *) * count));
+    if(!obj)
+        return NULL;
     obj->count = count;
 
     for(uint16_t i = 0; i < count; ++i) {
         struct field **field = &obj->items[i];
 
         *field = malloc(sizeof(struct field));
+        if(!*field)
+            goto fail;
 
         (*field)->access_flags = MemReader_next_uint16(reader);
         (*field)->name_index = MemReader_next_uint16(reader);
@@ -22,6 +26,10 @@ struct field_items *fields_parse(MemReader *reader) {
     }
 
     return obj;
+
+fail:
+    fields_free(obj);
+    return NULL;
 }
 
 static void field_free(struct field *this) {

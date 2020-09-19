8d118c61bc6a2a79b1cbdc067da9da3e54bf3066
Andrew Palmer <atp@sdf.org>
2020-09-19 18:29:21 -0500

    Apply mem.h to methods

--- src/core/methods.c
+++ src/core/methods.c
@@ -1,13 +1,13 @@
-#include <stdlib.h>
 #include "access.h"
 #include "attributes.h"
 #include "methods.h"
 #include "membuff.h"
+#include "mem.h"
 
 
 struct method_items *methods_parse(struct membuff *reader) {
     uint16_t count = membuff_next_uint16(reader);
-    struct method_items *obj = malloc(sizeof(struct method_items) + (sizeof(struct method *) * count));
+    struct method_items *obj = mem_malloc(sizeof(struct method_items) + (sizeof(struct method *) * count));
     if(!obj)
         return NULL;
     obj->count = count;
@@ -15,7 +15,7 @@ struct method_items *methods_parse(struct membuff *reader) {
     for(int i = 0; i < count; ++i) {
         struct method **method = &obj->items[i];
 
-        *method = malloc(sizeof(struct method));
+        *method = mem_malloc(sizeof(struct method));
         if(!*method)
             goto fail;
 
@@ -36,7 +36,7 @@ static void method_free(struct method *this) {
     if(!this)
         return;
     attributes_free(this->attributes);
-    free(this);
+    mem_free(this);
 }
 
 void methods_free(struct method_items *this) {
@@ -44,5 +44,5 @@ void methods_free(struct method_items *this) {
         return;
     for(uint16_t i = 0; i < this->count; ++i)
         method_free(this->items[i]);
-    free(this);
+    mem_free(this);
 }

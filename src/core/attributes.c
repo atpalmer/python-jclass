919938b05e1e321e099e30df1f049411c69f07f0
Andrew Palmer <atp@sdf.org>
2020-09-22 12:30:09 -0500

    Factor out _parse_attr_info

--- src/core/attributes.c
+++ src/core/attributes.c
@@ -4,6 +4,27 @@
 #include "membuff.h"
 #include "mem.h"
 
+static struct attribute *_parse_attr_info(struct membuff *reader, uint32_t len, uint16_t name_index, struct constant_pool *pool) {
+    struct attribute *new = mem_malloc(sizeof(struct attribute) + len);
+    if(!new)
+        return NULL;
+
+    struct pool_Utf8 *name = constant_pool_Utf8_item(pool, name_index);
+    if(!name) {
+        javaclass_error_set(JAVACLASS_ERR_PARSE, "Invalid attribute name index");
+        goto fail;
+    }
+
+    new->name_index = name_index;
+    new->length = len;
+    membuff_copy_next(reader, len, new->info);
+    return new;
+
+fail:
+    mem_free(new);
+    return NULL;
+}
+
 struct attribute_items *attributes_parse(struct membuff *reader, struct constant_pool *pool) {
     uint16_t count = membuff_next_uint16(reader);
     struct attribute_items *obj = mem_calloc(1, sizeof(struct attribute_items) + (sizeof(struct attribute *) * count));
@@ -15,21 +36,11 @@ struct attribute_items *attributes_parse(struct membuff *reader, struct constant
         struct attribute **attr = &obj->items[i];
 
         uint16_t name_index = membuff_next_uint16(reader);
-        if(!constant_pool_Utf8_item(pool, name_index)) {
-            javaclass_error_set(JAVACLASS_ERR_PARSE, "Invalid attribute name index");
-            goto fail;
-        }
-
         uint32_t length = membuff_next_uint32(reader);
 
-        *attr = mem_malloc(sizeof(struct attribute) + length);
+        *attr = _parse_attr_info(reader, length, name_index, pool);
         if(!*attr)
             goto fail;
-
-        (*attr)->name_index = name_index;
-        (*attr)->length = length;
-
-        membuff_copy_next(reader, length, (*attr)->info);
     }
 
     return obj;
